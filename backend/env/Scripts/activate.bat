5e9a0411888868042ae6e21db0dfee629a329e17
unknown <aurelien.destailleur@dc-digital.eu>
2022-04-17 11:48:25 +0200

    first step

--- backend/env/Scripts/activate.bat
+++ backend/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Brocolis\Desktop\MDS\MDProject\APP_FeatYou\backend\env
+set VIRTUAL_ENV=C:\Users\Destailleur Aurélien\Desktop\MDS\MyDigitalProject\APP_FeatYou\backend\env
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ frontend/src/components/NavBarView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-app-bar
+        elevation="4"
+        color="red"
+        > test </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default ({
+    data: () => ({
+        logo: require("@/assets/img/final.svg")
+    })
+})
+</script>
