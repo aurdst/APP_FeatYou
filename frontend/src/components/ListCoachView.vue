38e402029adde81200fd630db3a5a0de28569a4a
Max Maison <lefebvremaximee@gmail.com>
2022-06-07 12:13:15 +0200

    cache

--- frontend/src/components/ListCoachView.vue
+++ frontend/src/components/ListCoachView.vue
@@ -1,14 +1,17 @@
 <template>
   <v-card
-    max-width="450"
     class="mx-auto"
+    fluid
+    width="90%"
   >
     <v-spacer></v-spacer>
 
-    <v-btn class="btn_list ma-3" to="/conseils">
-      Voir tout nos coachs
+    <v-btn class="btn_list mt-3" text to="/">
+      Choisissez vôtre coach
       <v-icon
+        color="#DAAB39"
         dark
+        medium
         right
       >
         mdi-arrow-right
@@ -25,23 +28,27 @@
 
         <v-list-item
           :key="item"
+          class="css_item_coach"
         >
-          <!--
-            <v-list-item-avatar>
-              <v-img :src="item.avatar"></v-img>
-            </v-list-item-avatar>
-          -->
+          <v-list-item-avatar>
+            <!-- <v-img :src="item.avatar"></v-img> -->
+          </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title v-html="item.lastName + ' ' + item.firstName"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.sport"></v-list-item-subtitle>
+            <v-list-item-title class="txt_coach_preview" v-html="item.lastName + ' ' + item.name"></v-list-item-title>
+            <v-list-item-subtitle class="txt_coach_preview" v-html="item.sport"></v-list-item-subtitle>
           </v-list-item-content>
+
+          <br>
+
+          <v-btn @click="go_to_profile(item.id)" text color="#FFF" small :key="item" >
+            <v-icon small>mdi-arrow-right</v-icon>
+          </v-btn>
         </v-list-item>
 
-        
-          <v-btn @click="go_to_profile(item.id)" :key="item">
-            Voir profile
-          </v-btn>
+        <v-btn @click="go_to_profile(item.id)" :key="item">
+          Voir profile
+        </v-btn>
       </template>
     </v-list>
   </v-card>
@@ -61,14 +68,13 @@
     mounted: function() {
       //* If not connect
       if (this.$store.state.user.id == -1) {
-          router.push('/');
-          return
+        router.push('/');
+        return
       }
 
       //* Get user info
       this.$store.dispatch('getAllUserInfos').then(
         (rs) => {
-          console.log(rs)
           this.coachs = rs;
           
           return;
@@ -89,8 +95,8 @@
     methods : {
       go_to_profile(id) {
         router.push({
-          path   : 'coach_view',
-          query  : { id: id }
+          path  : 'coach_view',
+          query : { id: id }
         });
       }
     }
